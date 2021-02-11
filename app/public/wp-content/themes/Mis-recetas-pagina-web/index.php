2be99eb749d8f3ff231b8309b1d75190d08459ab
Edgarvlc <77021187+Edgarvlc@users.noreply.github.com>
2021-02-11 19:13:02 +0100

    inauguracion de la sesión

---
+++ app/public/wp-content/themes/Mis-recetas-pagina-web/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*$var1 = "virensys";
+$var2 = "curso";
+$var3 = "2021";
+
+echo "<div><p>Hola mundo ".$var1" ".$var2." ".$var3." </p></div>"
+
+echo date ("Y");*/
+
+echo "misrecetas.com - ".date("Y");
+
+
+?>
