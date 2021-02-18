4cf64a43e145bc947ba477122a1fb2f697c31d99
Edgarvlc <77021187+Edgarvlc@users.noreply.github.com>
2021-02-18 19:53:23 +0100

    Terminamos el examen final

--- app/public/wp-content/themes/Mis-recetas-pagina-web/header.php
+++ app/public/wp-content/themes/Mis-recetas-pagina-web/header.php
@@ -21,3 +21,46 @@
     </header>    
 
     <nav> Inicio | <a href="Recetas.html">Recetas</a> | <a href="Contactar.html">Contactar</a></nav>
+
+    <Main>
+        <section>
+          <article>
+              <h2>Receta 1</h2>
+              <img src="_resources/espaguetis-de-calabacin.jpg">
+              <br>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit.
+           </article>
+
+           <article>
+              <h2>Receta 2</h2>
+              <img src="_resources/lubina-con-hummus-de-guisantes.jpg">
+              <br>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            </article>
+
+            <article>
+              <h2>Receta 3</h2>
+              <img src="_resources/tiramisu-express.jpg">
+              <br>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </article>
+
+            <article>
+              <h2>Receta 4</h2>
+              <img src="_resources/vichyssoise-de-esparragos-blancos.jpg">
+              <br>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </article>
+        </section>
+
+        <section>
+          <h1>Menú del día</h1>
+              <ul>
+                  <li>Receta 6</li>
+                  <li>Receta 7</li>
+                  <li>Receta 8</li>
+              </ul>
+        </section>
+    </Main>
+
+</body>
